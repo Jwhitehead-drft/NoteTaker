a45f96dcea3c8262d99b4c60d6a4201da68f4664
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2021-03-01 14:31:35 -0500

    Fixed routes and prepped for heroku deployment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,12 @@ const path = require("path");
 
 
 module.exports = (app) => {
-    app.get('/notes', function(req, res) {
+    app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 }
 
